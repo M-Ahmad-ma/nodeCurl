0df7536a3478068a717ffb75bc6da048f431f704
Muhammad Ahmad <muhammadahmadma4642@gmail.com>
2025-12-22 10:47:40 +0000

    first commit

---
+++ bin/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+import { parseFlags } from "../lib/flags.js";
+import { ftpList } from "../lib/ftpHandler.js";
+import { expandLocalVars, isFileURL, isFtp } from "../lib/utils.js";
+import { readLocalFile, createFile, readHeaders } from "../lib/fileHandler.js";
+import { request } from "../lib/httpHandler.js";
+import { outPut } from "../lib/outputHandler.js";
+
+const args = process.argv.slice(2);
+const flags = parseFlags(args);
+
+let url = args.find(
+  (arg) =>
+    arg.startsWith("http://") ||
+    arg.startsWith("https://") ||
+    arg.startsWith("file://") ||
+    arg.startsWith("ftp://"),
+);
+
+if (!url) {
+  console.log("Usage: ./index.js [options] <url>");
+  process.exit(1);
+}
+
+url = expandLocalVars(url);
+
+if (flags.user === undefined) flags.user = "anonymous:anonymous";
+
+const main = async () => {
+
+
+  if (isFtp(url)) {
+    console.log("ftp connection");
+    console.log(url);
+    await ftpList(url, flags);
+    return;
+  }
+
+  if (isFileURL(url)) {
+    if (flags.upload) {
+      createFile(url, flags);
+    } else if (flags.head) {
+      await readHeaders(url, flags);
+    } else {
+      readLocalFile(url, flags);
+
+    }
+    process.exit(0);
+  }
+
+  let method = flags.method || (flags.upload ? "PUT" : "GET");
+  request(url, method, flags);
+};
+
+await main();
