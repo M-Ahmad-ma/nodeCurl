4590530008dd5b3acb7cc5819a58e33cc4c86cab
Muhammad Ahmad <muhammadahmadma4642@gmail.com>
2025-12-30 14:37:41 +0000

    added missings flags in currents modules

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 import { parseFlags } from "../lib/flags.js";
 import { ftpList } from "../lib/ftpHandler.js";
-import { expandLocalVars, isFileURL, isFtp } from "../lib/utils.js";
+import { expandLocalVars, isFileURL, isFtp, showHelp } from "../lib/utils.js";
 import { readLocalFile, createFile, readHeaders } from "../lib/fileHandler.js";
 import { request } from "../lib/httpHandler.js";
-import { outPut } from "../lib/outputHandler.js";
+// import { outPut } from "../lib/outputHandler.js";
 
 const args = process.argv.slice(2);
 const flags = parseFlags(args);
+const nonFlagArgs = args.filter((arg) => !arg.startsWith("-"));
+const module = nonFlagArgs[0];
+
+if (flags.help) {
+  showHelp(module);
+  process.exit(0);
+}
 
 let url = args.find(
   (arg) =>
@@ -18,7 +25,7 @@ let url = args.find(
 );
 
 if (!url) {
-  console.log("Usage: ./index.js [options] <url>");
+  console.log("Error: Usage: ./index.js [options] <url>");
   process.exit(1);
 }
 
@@ -27,11 +34,7 @@ url = expandLocalVars(url);
 if (flags.user === undefined) flags.user = "anonymous:anonymous";
 
 const main = async () => {
-
-
   if (isFtp(url)) {
-    console.log("ftp connection");
-    console.log(url);
     await ftpList(url, flags);
     return;
   }
@@ -43,7 +46,6 @@ const main = async () => {
       await readHeaders(url, flags);
     } else {
       readLocalFile(url, flags);
-
     }
     process.exit(0);
   }
